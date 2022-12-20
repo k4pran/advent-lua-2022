cc72563d5ba32798b4cc1a333e6ecdfcc9c1ef87
Ryan McCauley <ryanmccauley211@gmail.com>
2022-12-20 07:40:59 +0000

    Uncomment

--- day20.py
+++ day20.py
@@ -1,29 +1,29 @@
 
-# # p1
-# with open("resources/day20.txt", 'r') as f:
-#     code = [i for i in map(int, f.read().splitlines())]
-#     encryption = [(k, v) for k, v in enumerate(code)]
-#
-#     index = 0
-#     print([i[1] for i in encryption])
-#     for n, movement in enumerate(code):
-#
-#         for j in range(len(code)):
-#             if encryption[j][0] == n:
-#                 index = j
-#                 break
-#
-#         distance = encryption[j][1]
-#         encryption.insert(((index + distance)) % (len(encryption) - 1), encryption.pop(index % len(encryption)))
-#         print([i[1] for i in encryption])
-#
-#
-#     # find 0 index
-#     for i, val in enumerate(encryption):
-#         if val[1] == 0:
-#             break
-#     groove = encryption[(i + 1000) % len(encryption)][1] + encryption[(i + 2000) % len(encryption)][1] + encryption[(i + 3000) % len(encryption)][1]
-#     print(groove)
+# p1
+with open("resources/day20.txt", 'r') as f:
+    code = [i for i in map(int, f.read().splitlines())]
+    encryption = [(k, v) for k, v in enumerate(code)]
+
+    index = 0
+    print([i[1] for i in encryption])
+    for n, movement in enumerate(code):
+
+        for j in range(len(code)):
+            if encryption[j][0] == n:
+                index = j
+                break
+
+        distance = encryption[j][1]
+        encryption.insert(((index + distance)) % (len(encryption) - 1), encryption.pop(index % len(encryption)))
+        print([i[1] for i in encryption])
+
+
+    # find 0 index
+    for i, val in enumerate(encryption):
+        if val[1] == 0:
+            break
+    groove = encryption[(i + 1000) % len(encryption)][1] + encryption[(i + 2000) % len(encryption)][1] + encryption[(i + 3000) % len(encryption)][1]
+    print(groove)
 
 #p2
 with open("resources/day20.txt", 'r') as f:
